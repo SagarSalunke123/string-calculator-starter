4dd71144bd84bf8f1d50b50092670afa2ae94bdb
D8_49590_Sagar <sagar2thebest@gmail.com>
2021-10-17 16:18:06 +0530

    2nd test string_with_single_number_should_return_number_as_int done success

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,9 +4,27 @@ class StringCalculator {
 
     public int add(String input) {
         char [] in= input.toCharArray();
-        int size=in.length, sum=0;
+        int size=in.length, sum=0, i = 0, x = 0;
         if(size==0)
             return sum;
-        return 0;
+        for (i = 0; i < size; i++) {
+            int temp = 0;
+            if (Character.isDigit(in[i])) {
+                temp += Character.digit(in[i], 10);
+                for (x = i + 1; x < size; x++) {
+                    if (Character.isDigit(in[x])) {
+                        temp = (temp * 10) + Character.digit(in[x], 10);
+                    } else
+                        break;
+                }
+                if (x != 0) {
+                    i = x - 1;
+                    x = 0;
+                }
+                if (temp <= 1000)
+                    sum += temp;
+            }
+        }
+        return sum;
     }
 }
